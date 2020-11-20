cebe86de21e44db9fe252645511192a9747687be
Joe Napper <joe.napper@cmisukltd.onmicrosoft.com>
2020-11-20 23:23:38 +0000

    Using redux to store data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import "./";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div className="App">
       <h1>Ignite</h1>
---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,24 @@
+const { VisibilityAction } = require("framer-motion");
+
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+  searches: [],
+};
+
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      };
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
