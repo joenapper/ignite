42bc24eea06292375d4b5bed899b0818845dfc5e
Joe Napper <joe.napper@cmisukltd.onmicrosoft.com>
2020-11-21 00:00:44 +0000

    Rendering data on screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/home";
 function App() {
   return (
     <div className="App">
-      <h1>Ignite</h1>
       <Home />
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,42 @@
 import { useEffect } from "react";
 // Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+// Components
+import Game from "../components/Game";
+// Styling & Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   // Fetch Games
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  // Extract Data
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)``;
+
+const Games = styled(motion.div)``;
 
 export default Home;
